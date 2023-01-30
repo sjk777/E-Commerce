2769c026ccddb09df97b3034c96afa2e18018d09
Sky K <skyj.kim7@gmail.com>
2023-01-30 09:22:53 -0800

    updated models v2

--- models/Tag.js
+++ models/Tag.js
@@ -8,13 +8,13 @@ class Tag extends Model {}
 Tag.init(
   {
     id:{
-      type: DataType.INTEGER,
+      type: DataTypes.INTEGER,
       allowNull: false,
       primaryKey: true,
       autoIncrement: true,
     },
     tag_name:{
-      type:DataType.STRING,
+      type:DataTypes.STRING,
     },
   },
   {
